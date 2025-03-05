bcc7a9246e3615e77af44b9c3a6db905898b452c
sunbaolin <baolin.sun.wang@gmail.com>
2025-03-05 10:51:47 +0800

    add focus imaging B-mode video generation function

---
+++ focus_imaging_video.py
@@ -0,0 +1,79 @@
+import os
+import cv2
+import time
+import torch
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from scipy.interpolate import griddata
+from datasets.FocusedTxData import WUSData
+from beamforming.DAS import DAS_FT, DAS_FT_B
+from beamforming.PixelGrid import make_foctx_grid, make_pixel_grid
+
+F = WUSData('configs\linear_array_2.5M.json')
+
+rmax = F.scan_depth
+wvln = F.c / F.fc
+dr = wvln / 4
+rlims = [0, rmax]
+scan_grid = make_foctx_grid(rlims, dr, F.rx_ori, F.rx_dir)
+scan_convert = True
+drange = F.drange
+fnum = F.fnum
+
+das = DAS_FT_B(F, scan_grid, rxfnum=fnum)
+
+for i in tqdm(range(18)):
+    rfdata = []
+    for j in range(F.nxmits):
+        df = pd.read_csv(f'rfdata/rfdata_{i+1}_{j+1}.csv', sep=',', header=None)
+        data = df.values
+        data = (data - 512) / 512
+        data = data.T
+        rfdata.append(data)
+
+    rfdata = np.array(rfdata)
+    F.load_data(rfdata)
+
+    idata = torch.tensor(F.idata, dtype=torch.float, device=torch.device("cuda:0"))
+    qdata = torch.tensor(F.qdata, dtype=torch.float, device=torch.device("cuda:0"))
+    x = (idata, qdata)
+
+    # start_time = time.time()
+    # idas, qdas = das(x)
+    # iq = idas + 1j * qdas
+    # bimg = torch.abs(iq).T
+
+    # start_time = time.time()
+    idas, qdas = das(x)
+    idas, qdas = idas.detach().cpu().numpy(), qdas.detach().cpu().numpy()
+    iq = idas + 1j * qdas
+    bimg = np.abs(iq).T
+
+
+
+    # Scan convert if necessary
+    if scan_convert:
+        xlims = rlims[1] * np.array([-0.7, 0.7])
+        zlims = rlims[1] * np.array([0, 1])
+        img_grid = make_pixel_grid(xlims, zlims, wvln / 2, wvln / 2)
+        grid = np.transpose(scan_grid, (1, 0, 2))
+        g1 = np.stack((grid[:, :, 2], grid[:, :, 0]), -1).reshape(-1, 2)
+        g2 = np.stack((img_grid[:, :, 2], img_grid[:, :, 0]), -1).reshape(-1, 2)
+        bsc = griddata(g1, bimg.reshape(-1), g2, "linear", 1e-10)
+        bimg = np.reshape(bsc, img_grid.shape[:2])
+        grid = img_grid.transpose(1, 0, 2)
+
+    # print(time.time() - start_time)
+
+    bimg = 20 * np.log10(bimg)  # Log-compress
+    bimg -= np.amax(bimg)  # Normalize by max value
+
+    # Display images via matplotlib
+    extent = [grid[0, 0, 0], grid[-1, 0, 0], grid[0, -1, 2], grid[0, 0, 2]]
+    extent = np.array(extent) * 1e3  # Convert to mm
+    plt.imshow(bimg, vmin=-drange, cmap="gray", extent=extent, origin="upper")
+    plt.axis('off')
+    plt.savefig(os.path.join('usimage', f"./usImage{i+1}.jpg"), bbox_inches='tight', pad_inches=0)
+    # plt.show()
---
+++ utils/make_video.py
@@ -0,0 +1,58 @@
+import cv2
+import os
+from natsort import natsorted
+
+def images_to_video(image_folder, output_video, fps=30, frame_size=None):
+    """
+    将文件夹内的图片制作成视频。
+    
+    :param image_folder: 包含图片的文件夹路径
+    :param output_video: 输出视频文件的路径
+    :param fps: 视频的帧率
+    :param frame_size: 视频帧的大小（宽, 高），默认为None，根据第一张图片自动设置
+    """
+    # 获取文件夹内所有图片文件并排序
+    images = [img for img in os.listdir(image_folder) if img.endswith((".png", ".jpg", ".jpeg"))]
+    images = natsorted(images)  # 自然排序，确保图片按照数字命名顺序排列
+    
+    if not images:
+        print("没有找到图片文件")
+        return
+
+    # 获取第一张图片以确定帧的大小
+    first_image_path = os.path.join(image_folder, images[0])
+    first_frame = cv2.imread(first_image_path)
+    if frame_size is None:
+        frame_size = (first_frame.shape[1], first_frame.shape[0])  # (宽, 高)
+
+    # 初始化视频写入对象
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # 使用 mp4 编码
+    out = cv2.VideoWriter(output_video, fourcc, fps, frame_size)
+
+    x1, y1, x2, y2 = 30, 20, 226, 256  # 裁剪区域的左上角和右下角坐标
+
+    for i in range(10):
+        for image_name in images:
+            image_path = os.path.join(image_folder, image_name)
+            frame = cv2.imread(image_path)
+
+            # frame = cv2.GaussianBlur(frame, (5, 5), sigmaX=1.0)
+            # frame = frame[y1:y2, x1:x2]
+
+
+            # 检查图像大小是否与指定的帧大小一致
+            if (frame.shape[1], frame.shape[0]) != frame_size:
+                frame = cv2.resize(frame, frame_size)
+
+            out.write(frame)  # 写入帧到视频文件
+
+    out.release()
+    print(f"视频已保存到 {output_video}") 
+
+
+# 示例用法
+if __name__ == "__main__":
+    image_folder = "usimage"  # 替换为你的图片文件夹路径
+    output_video = "usvideo/output21.mp4"          # 输出视频文件名
+    fps = 5                                  # 帧率
+    images_to_video(image_folder, output_video, fps)
